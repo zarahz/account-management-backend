8e814386dde70e08c844cb1e65c3455790a1ccb9
Blackpearl <zarah.zahreddin@gmail.com>
2019-12-19 17:59:48 +0100

    temporarily enabled cors for all

--- src/server.js
+++ src/server.js
@@ -6,13 +6,13 @@ require('./db');
 const app = express();
 const port = 10014;
 
-const corsOptions = {
+/* const corsOptions = {
   origin: 'https://pwp.um.ifi.lmu.de',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+}; */
 
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+app.use(cors());// app.use(cors(corsOptions));
 app.get('/', async (req, res) => { res.sendFile('views/Welcome.html', { root: __dirname }); });
 
 // user API
