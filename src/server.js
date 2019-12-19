bad31ce03a52c6e0b0a6e62830a5eb240bcfd51c
Blackpearl <zarah.zahreddin@gmail.com>
2019-12-19 20:31:45 +0100

    cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 require('./db');
 
 const app = express();
