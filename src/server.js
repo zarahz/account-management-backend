79d9e2bed74a8940f8bf11508da945080a16d642
Blackpearl <zarah.zahreddin@gmail.com>
2019-12-19 17:11:20 +0100

    should fix cors issue

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('./db');
 
 const app = express();
 const port = 10014;
 
+const corsOptions = {
+  origin: 'https://pwp.um.ifi.lmu.de',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 app.get('/', async (req, res) => { res.sendFile('views/Welcome.html', { root: __dirname }); });
 
 // user API
