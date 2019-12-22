084fdbf6bafd7a2d4a42af2d12d26ffffd0a6f04
Blackpearl <zarah.zahreddin@gmail.com>
2019-12-22 12:10:18 +0100

    cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 require('./db');
 
 const app = express();
@@ -11,22 +11,9 @@ const port = 10014;
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }; */
 // cors settings
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  );
-  next();
-});
-
-// All OPTIONS requests return a simple status: 'OK'
-app.options('*', (req, res) => {
-  res.json({
-    status: 'OK',
-  });
-});
+app.use(cors({
+  origin: '*',
+}));
 
 app.use(bodyParser.json());
 app.get('/', async (req, res) => { res.sendFile('views/Welcome.html', { root: __dirname }); });
