53c054fb605e62fdee4887c8329050a7d2156b75
alexander <alexander.zwermann@horizon-alpha.com>
2019-12-23 14:33:45 +0100

    route securityQuestion get paramter languageCode and sends the sentences in englisch or german back

--- src/routes/collection.js
+++ src/routes/collection.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { securityQuestions, researchInterests } = require('../util/enums');
+const { securityQuestionsEN, securityQuestionsDE, researchInterests } = require('../util/enums');
 
 const router = express.Router();
 
-router.get('/securityQuestions', (req, res) => { res.send(securityQuestions); });
+router.get('/securityQuestions', (req, res) => {
+  const { languageCode } = req.body;
+  if (languageCode === 'de') {
+    res.send(securityQuestionsDE);
+  } else if (languageCode === 'en') {
+    res.send(securityQuestionsEN);
+  }
+});
 
 router.get('/researchInterests', (req, res) => { res.send(researchInterests); });
 
--- src/util/enums.js
+++ src/util/enums.js
@@ -1,4 +1,4 @@
-const securityQuestions = ['What was the house number and street name you lived in as a child?',
+const securityQuestionsEN = ['What was the house number and street name you lived in as a child?',
   'What were the last four digits of your childhood telephone number?',
   'What primary school did you attend?',
   'In what town or city was your first full time job?',
@@ -9,11 +9,23 @@ const securityQuestions = ['What was the house number and street name you lived 
   "What is your spouse or partner's mother's maiden name?",
   'In what town or city did your mother and father meet?'];
 
+const securityQuestionsDE = ['Wie war die Hausnummer und der Straßenname, in dem Sie als Kind gelebt haben?',
+  'Was waren die letzten vier Ziffern der Telefonnummer Ihrer Kindheit?',
+  'Welche Grundschule haben Sie besucht?',
+  'In welcher Stadt war Ihr erster Vollzeitjob?',
+  'In welcher Stadt haben Sie Ihren Ehepartner/Partner getroffen?',
+  'Wie lautet der zweite Vorname Ihres ältesten Kindes?',
+  'Was sind die letzten fünf Ziffern Ihrer Führerscheinnummer?',
+  'Wie lautet der Mädchenname Ihrer Großmutter (mütterlicherseits)?',
+  'Wie lautet der Mädchenname der Mutter Ihres Ehepartners oder Ihrer Partnerin?',
+  'In welcher Stadt lernten sich deine Mutter und dein Vater kennen?'];
+
 const researchInterests = [
   'VR', 'AR', 'AV',
 ];
 
 module.exports = {
-  securityQuestions,
+  securityQuestionsEN,
+  securityQuestionsDE,
   researchInterests,
 };
