ea8835c8c536bbbabb7c9d81c295b9ed8be9b93b
Blackpearl <zarah.zahreddin@gmail.com>
2020-01-06 20:27:33 +0100

    adapted cors

--- src/server.js
+++ src/server.js
@@ -13,15 +13,16 @@ app.use(morgan('combined'));
   origin: '*',
 })); */
 const whitelist = ['https://pwp.um.ifi.lmu.de', 'http://localhost:3000'];
-const checkUrl = (origin, callback) => {
-  console.log(origin);
-  if (whitelist.indexOf(origin) !== -1) {
-    callback(null, true);
-  } else {
-    callback(new Error('Not allowed by CORS'));
-  }
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
 };
-app.use(cors({ credentials: true, origin: checkUrl }));
+app.use(cors({ credentials: true, origin: corsOptions }));
 
 app.use(bodyParser.json());
 
